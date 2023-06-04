d0e63948639505997d48f8abb8a9e4e2fcf70f43
Jose Peña <josepetew@gmail.com>
2023-06-04 16:34:26 +0200

    feat(category): implement order by popularity

--- src/types/generated-graphql-types.ts
+++ src/types/generated-graphql-types.ts
@@ -6,6 +6,7 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string | number; output: string; }
@@ -15,7 +16,13 @@ export type Scalars = {
   Float: { input: number; output: number; }
 };
 
+export enum CacheControlScope {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC'
+}
+
 export type CategoriesOrderBy = {
+  popularity?: InputMaybe<Sort>;
   title?: InputMaybe<Sort>;
 };
 
@@ -23,13 +30,51 @@ export type Category = {
   __typename?: 'Category';
   createdAt: Scalars['String']['output'];
   id: Scalars['ID']['output'];
+  products: Array<Product>;
   slug: Scalars['String']['output'];
   title: Scalars['String']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createCategory: Category;
+  createProduct: Product;
+};
+
+
+export type MutationCreateCategoryArgs = {
+  title: Scalars['String']['input'];
+};
+
+
+export type MutationCreateProductArgs = {
+  categoryId: Scalars['Int']['input'];
+  price: Scalars['Float']['input'];
+  title: Scalars['String']['input'];
+};
+
+export type Product = {
+  __typename?: 'Product';
+  category: Category;
+  createdAt: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  price: Scalars['Float']['output'];
+  slug: Scalars['String']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type ProductFilterBy = {
+  categoryId: Scalars['ID']['input'];
+};
+
+export type ProductsOrderBy = {
+  createdAt?: InputMaybe<Sort>;
 };
 
 export type Query = {
   __typename?: 'Query';
   categories: Array<Category>;
+  products: Array<Product>;
 };
 
 
@@ -39,10 +84,23 @@ export type QueryCategoriesArgs = {
   orderBy?: InputMaybe<CategoriesOrderBy>;
 };
 
+
+export type QueryProductsArgs = {
+  filterBy?: InputMaybe<ProductFilterBy>;
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<ProductsOrderBy>;
+};
+
 export enum Sort {
   Asc = 'asc',
   Desc = 'desc'
 }
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  productAdded: Product;
+};
 
 export type WithIndex<TObject> = TObject & Record<string, any>;
 export type ResolversObject<TObject> = WithIndex<TObject>;
@@ -117,13 +175,20 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
+  CacheControlScope: CacheControlScope;
   CategoriesOrderBy: CategoriesOrderBy;
   Category: ResolverTypeWrapper<Category>;
+  Float: ResolverTypeWrapper<Scalars['Float']['output']>;
   ID: ResolverTypeWrapper<Scalars['ID']['output']>;
   Int: ResolverTypeWrapper<Scalars['Int']['output']>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Product: ResolverTypeWrapper<Product>;
+  ProductFilterBy: ProductFilterBy;
+  ProductsOrderBy: ProductsOrderBy;
   Query: ResolverTypeWrapper<{}>;
   Sort: Sort;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
+  Subscription: ResolverTypeWrapper<{}>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
@@ -131,15 +196,45 @@ export type ResolversParentTypes = ResolversObject<{
   Boolean: Scalars['Boolean']['output'];
   CategoriesOrderBy: CategoriesOrderBy;
   Category: Category;
+  Float: Scalars['Float']['output'];
   ID: Scalars['ID']['output'];
   Int: Scalars['Int']['output'];
+  Mutation: {};
+  Product: Product;
+  ProductFilterBy: ProductFilterBy;
+  ProductsOrderBy: ProductsOrderBy;
   Query: {};
   String: Scalars['String']['output'];
+  Subscription: {};
 }>;
+
+export type CacheControlDirectiveArgs = {
+  inheritMaxAge?: Maybe<Scalars['Boolean']['input']>;
+  maxAge?: Maybe<Scalars['Int']['input']>;
+  scope?: Maybe<CacheControlScope>;
+};
+
+export type CacheControlDirectiveResolver<Result, Parent, ContextType = any, Args = CacheControlDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
 
 export type CategoryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Category'] = ResolversParentTypes['Category']> = ResolversObject<{
   createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  products?: Resolver<Array<ResolversTypes['Product']>, ParentType, ContextType>;
+  slug?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  createCategory?: Resolver<ResolversTypes['Category'], ParentType, ContextType, RequireFields<MutationCreateCategoryArgs, 'title'>>;
+  createProduct?: Resolver<ResolversTypes['Product'], ParentType, ContextType, RequireFields<MutationCreateProductArgs, 'categoryId' | 'price' | 'title'>>;
+}>;
+
+export type ProductResolvers<ContextType = any, ParentType extends ResolversParentTypes['Product'] = ResolversParentTypes['Product']> = ResolversObject<{
+  category?: Resolver<ResolversTypes['Category'], ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  price?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
   slug?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
@@ -147,10 +242,21 @@ export type CategoryResolvers<ContextType = any, ParentType extends ResolversPar
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
   categories?: Resolver<Array<ResolversTypes['Category']>, ParentType, ContextType, Partial<QueryCategoriesArgs>>;
+  products?: Resolver<Array<ResolversTypes['Product']>, ParentType, ContextType, Partial<QueryProductsArgs>>;
+}>;
+
+export type SubscriptionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Subscription'] = ResolversParentTypes['Subscription']> = ResolversObject<{
+  productAdded?: SubscriptionResolver<ResolversTypes['Product'], "productAdded", ParentType, ContextType>;
 }>;
 
 export type Resolvers<ContextType = any> = ResolversObject<{
   Category?: CategoryResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Product?: ProductResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
+  Subscription?: SubscriptionResolvers<ContextType>;
 }>;
 
+export type DirectiveResolvers<ContextType = any> = ResolversObject<{
+  cacheControl?: CacheControlDirectiveResolver<any, any, ContextType>;
+}>;
